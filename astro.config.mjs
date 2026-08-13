eaffb0896ffb18f691ed9cc5cf0208bd638e299e
Salehuddin <23058932+salehuddin@users.noreply.github.com>
2026-08-13 22:17:32 +0800

    chore: switch deploy to Replit via SSH

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teleprompter.my',
-  base: '/preview/',
+  base: '/',
   output: 'static',
   integrations: [sitemap()],
   build: {
